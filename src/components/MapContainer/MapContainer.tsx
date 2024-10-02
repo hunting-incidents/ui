c51e52f91d4a3eada5752ff91a446f23085828f9
wolf <7438385+wolf29f@users.noreply.github.com>
2024-10-02 23:42:25 +0200

    feat: add basic map using react-leaflet

---
+++ src/components/MapContainer/MapContainer.tsx
@@ -0,0 +1,41 @@
+import "../../App.css";
+import "leaflet/dist/leaflet.css";
+import { useRef } from "react";
+
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+export default () => {
+  const tileLAyerRef = useRef(null);
+
+  if (tileLAyerRef.current) {
+    console.log(tileLAyerRef.current);
+  }
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "500px", width: "100%" }}
+        attributionControl={false}
+      >
+        <TileLayer
+          ref={tileLAyerRef}
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
