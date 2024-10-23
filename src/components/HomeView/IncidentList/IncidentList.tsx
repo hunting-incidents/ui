d7c081831b597fb58073090d05921f85efa51a29
wolf <7438385+wolf29f@users.noreply.github.com>
2024-10-24 00:00:51 +0200

    feat: work on styling

--- src/components/HomeView/IncidentList/IncidentList.tsx
+++ src/components/HomeView/IncidentList/IncidentList.tsx
@@ -1,25 +1,62 @@
+import { useContext } from "react";
 import { Incident } from "../../../api/incidents";
+import "./IncidentList.css";
+import {
+  GiBrokenBone,
+  GiGraveFlowers,
+  GiSawedOffShotgun,
+  GiShatteredGlass,
+} from "react-icons/gi";
+import { ConfigContext } from "../../ConfigProvider/ConfigProvider";
 
 interface IncidentListProps {
   incidents: Incident[];
 }
 
 function IncidentList({ incidents }: IncidentListProps) {
+  const config = useContext(ConfigContext);
+
+  function TypeToIcon(type: number) {
+    const name = config.incident_types.find((t) => t.id === type)?.name;
+
+    switch (name?.toLowerCase()) {
+      case "mort":
+        return (
+          <GiGraveFlowers size={"2rem"} style={{ marginRight: ".5rem" }} />
+        );
+      case "menace":
+        return (
+          <GiSawedOffShotgun size={"2rem"} style={{ marginRight: ".5rem" }} />
+        );
+      case "blessure":
+        return <GiBrokenBone size={"2rem"} style={{ marginRight: ".5rem" }} />;
+      case "destruction":
+        return (
+          <GiShatteredGlass size={"2rem"} style={{ marginRight: ".5rem" }} />
+        );
+      default:
+        return null;
+    }
+  }
+
   return (
     <div className="incident-list">
       Résultats de la recherche
-      <div
+      <ul
         style={{
           textAlign: "left",
         }}
       >
         {incidents.map((incident) => (
-          <div key={incident.id}>
-            <h3>{incident.title}</h3>
+          <li key={incident.id}>
+            <h3>
+              {TypeToIcon(incident.incident_type_id)}
+              {incident.title}
+            </h3>
             <p>{incident.description}</p>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
